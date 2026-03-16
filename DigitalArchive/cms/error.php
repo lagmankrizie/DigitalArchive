df89550b3e2fe3dc861e7ebb7646e153895dfebe
lagmankrizie <lagmankrizie@gmail.com>
2026-03-16 19:35:42 +0800

    Add files via upload

---
+++ DigitalArchive/cms/error.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>An error occurred</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
+  </head>
+  <body>
+
+    <h1 class="logo">Digital Archive</h1>
+
+    <h1>Sorry! An error occurred.</h1>
+    <p>The site owners have been notified and will fix the problem as soon as possible.</p>
+    <p><a href="index.php">Click here to go back to the home page</a>.</p>
+    <p>If the problem persists <a href="mailto:digitalarchive@gmail.com">click here to email us</a>.</p>
+
+  </body>
+</html>
---
+++ DigitalArchive/cms/includes/admin-footer.php
@@ -0,0 +1,8 @@
+  <footer>
+      <div class="container">
+        &copy; Digital Archive <?= date('Y'); ?>
+      </div>
+    </footer>
+    <script src="../js/site.js"></script>
+  </body>
+</html>
